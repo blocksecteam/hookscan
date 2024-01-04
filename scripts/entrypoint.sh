079881f441e87dc1d3235c24cd7dc9f3c15f7c3a
xiyao <futuretech6@foxmail.com>
2024-01-04 18:32:19 +0800

    fix: update entrypoint
    1. rewrite array append
    2. add external args from docker run

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,12 +2,20 @@
 
 PROJECT_PATH=/project
 
-ARGS=(--base-path $PROJECT_PATH $PROJECT_PATH/$CONTRACT)
+# set base path
+ARGS=(--base-path $PROJECT_PATH)
 
+# set solc bin path
 if [ "${SOLC_VERSION:=""}" ]; then
    SOLC_VERSION=0.8.19
 fi
-ARGS=(--solc-bin $SOLC_PATH/v$SOLC_VERSION/solc ${ARGS[@]})
+ARGS+=(--solc-bin $SOLC_PATH/v$SOLC_VERSION/solc)
+
+# add external args
+ARGS+=($@)
+
+# set the input contract
+ARGS+=($PROJECT_PATH/$CONTRACT)
 
 echo "arg list: ${ARGS[@]}"
 
